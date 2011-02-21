25ba19f222872c790a4900a02cfec0831394296c
Aitor Fernández <af@aitorfernandez.com>
2011-02-22 00:14:33 +0100

    Add Utils and definition of the particle class

--- Classes/SpringParticlesAppDelegate.m
+++ Classes/SpringParticlesAppDelegate.m
@@ -10,7 +10,7 @@
 
 #import "SpringParticlesAppDelegate.h"
 #import "GameConfig.h"
-#import "HelloWorldScene.h"
+#import "MainScene.h"
 #import "RootViewController.h"
 
 @implementation SpringParticlesAppDelegate
@@ -110,7 +110,7 @@
 	[self removeStartupFlicker];
 	
 	// Run the intro Scene
-	[[CCDirector sharedDirector] runWithScene: [HelloWorld scene]];		
+	[[CCDirector sharedDirector] runWithScene: [MainScene scene]];		
 }
 
 
